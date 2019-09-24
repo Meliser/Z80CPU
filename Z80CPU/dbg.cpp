17a0f01410f51ab458fb5009b9b4fcac39a92c63
Meliser <mitya.korneev@mail.ru>
2019-09-24 15:02:31 +0000

    add additional registers dump

--- Z80CPU/dbg.cpp
+++ Z80CPU/dbg.cpp
@@ -4,87 +4,99 @@ void registersDump(Z80Cpu* z80Cpu) {
 
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	static unsigned char previous8[10] = { 0 };
-	static unsigned short previous16[4] = { 0 };
-	//remove current
-	/*unsigned char current8[10] =
+	static unsigned char previousGp[GP_REGISTERS_SIZE] = { 0 };
+	static unsigned char previousAddGp[GP_REGISTERS_SIZE] = { 0 };
+	static unsigned char previousSp8[SP_REGISTERS8_SIZE] = { 0 };
+	static unsigned short previousSp16[SP_REGISTERS16_SIZE] = { 0 };
+	
+	unsigned char changedGp[GP_REGISTERS_SIZE] =
 	{
-		z80Cpu->basicGpRegisters[B],	z80Cpu->basicGpRegisters[C],
-		z80Cpu->basicGpRegisters[D],	z80Cpu->basicGpRegisters[E],
-		z80Cpu->basicGpRegisters[H],	z80Cpu->basicGpRegisters[L],
-		z80Cpu->basicGpRegisters[F],	z80Cpu->basicGpRegisters[A],
-		z80Cpu->spRegisters8[I],		z80Cpu->spRegisters8[R]
-	};*/
-	/*unsigned short current16[4] =
-	{
-		z80Cpu->spRegisters16[IX],z80Cpu->spRegisters16[IY],
-		z80Cpu->spRegisters16[SP],z80Cpu->spRegisters16[PC]
-	};*/
-
-	unsigned char changed8[10] =
-	{
-	z80Cpu->basicGpRegisters[B] ^ previous8[B],
-	z80Cpu->basicGpRegisters[C] ^ previous8[C],
-	z80Cpu->basicGpRegisters[D] ^ previous8[D],
-	z80Cpu->basicGpRegisters[E] ^ previous8[E],
-	z80Cpu->basicGpRegisters[H] ^ previous8[H],
-	z80Cpu->basicGpRegisters[L] ^ previous8[L],
-	z80Cpu->basicGpRegisters[F] ^ previous8[F],
-	z80Cpu->basicGpRegisters[A] ^ previous8[A],
-	z80Cpu->spRegisters8[I] ^ previous8[I],
-	z80Cpu->spRegisters8[R] ^ previous8[R]
+		z80Cpu->basicGpRegisters[B] ^ previousGp[B],
+		z80Cpu->basicGpRegisters[C] ^ previousGp[C],
+		z80Cpu->basicGpRegisters[D] ^ previousGp[D],
+		z80Cpu->basicGpRegisters[E] ^ previousGp[E],
+		z80Cpu->basicGpRegisters[H] ^ previousGp[H],
+		z80Cpu->basicGpRegisters[L] ^ previousGp[L],
+		z80Cpu->basicGpRegisters[F] ^ previousGp[F],
+		z80Cpu->basicGpRegisters[A] ^ previousGp[A],
 	};
-	unsigned short changed16[4] =
+	unsigned char changedAddGp[GP_REGISTERS_SIZE] =
 	{
-	z80Cpu->spRegisters16[IX] ^ previous16[IX],
-	z80Cpu->spRegisters16[IY] ^ previous16[IY],
-	z80Cpu->spRegisters16[SP] ^ previous16[SP],
-	z80Cpu->spRegisters16[PC] ^ previous16[PC]
+		z80Cpu->additionalGpRegisters[B] ^ previousAddGp[B],
+		z80Cpu->additionalGpRegisters[C] ^ previousAddGp[C],
+		z80Cpu->additionalGpRegisters[D] ^ previousAddGp[D],
+		z80Cpu->additionalGpRegisters[E] ^ previousAddGp[E],
+		z80Cpu->additionalGpRegisters[H] ^ previousAddGp[H],
+		z80Cpu->additionalGpRegisters[L] ^ previousAddGp[L],
+		z80Cpu->additionalGpRegisters[F] ^ previousAddGp[F],
+		z80Cpu->additionalGpRegisters[A] ^ previousAddGp[A],
+	};
+	unsigned char changedSp8[SP_REGISTERS8_SIZE] =
+	{
+		z80Cpu->spRegisters8[I] ^ previousGp[I],
+		z80Cpu->spRegisters8[R] ^ previousGp[R]
+	};
+	unsigned short changedSp16[SP_REGISTERS16_SIZE] =
+	{
+	z80Cpu->spRegisters16[IX] ^ previousSp16[IX],
+	z80Cpu->spRegisters16[IY] ^ previousSp16[IY],
+	z80Cpu->spRegisters16[SP] ^ previousSp16[SP],
+	z80Cpu->spRegisters16[PC] ^ previousSp16[PC]
 	};
 
-	static char gpMnem[6] = { 'B','C','D','E','H','L' };
-	static char gpAddMnem[6] = { 'B\'','C\'','D\'','E\'','H\'','L\'' };
+	static char gpMnem[GP_REGISTERS_SIZE-2] = { 'B','C','D','E','H','L' };
+
 	WORD color;
-	for (size_t i = 0; i < 6; i++)
+	printf("\n\n");
+	for (size_t i = 0; i < GP_REGISTERS_SIZE-2; i++)
 	{
-		color = changed8[i] ? GREEN : GREY;
+		color = changedGp[i] ? GREEN : GREY;
 		SetConsoleTextAttribute(hConsole, color);
-		if(i % 2 == 0) {
+		printf("%c = 0x%02X \t", gpMnem[i], z80Cpu->basicGpRegisters[i]);
+		if(i % 2 == 1) {
+			for (size_t j = 0; j < 2; j++)
+			{
+				color = changedAddGp[i] ? GREEN : GREY;
+				SetConsoleTextAttribute(hConsole, color);
+				printf("%c' = 0x%02X \t", gpMnem[i], z80Cpu->additionalGpRegisters[i]);
+			}
 			printf("\n\n");
 		}
-		printf("%c = 0x%02X \t", gpMnem[i], z80Cpu->basicGpRegisters[i]);
 	}
-	printf("\n\n");
 
 	static const char* flagMnem[8] = { "C","N","P/V","-","H","-","Z","S" };
 	for (size_t i = 0; i < 8; i++)
 	{
-		color = (changed8[F] >> i) & 1 ? GREEN : GREY;
+		color = (changedGp[F] >> i) & 1 ? GREEN : GREY;
 		SetConsoleTextAttribute(hConsole, color);
-		printf("%-3s = %d \n", flagMnem[i], (z80Cpu->basicGpRegisters[F] >> i) & 1);
+		printf("%-3s = %d \t\t\t", flagMnem[i], (z80Cpu->basicGpRegisters[F] >> i) & 1);
+
+		color = (changedAddGp[F] >> i) & 1 ? GREEN : GREY;
+		SetConsoleTextAttribute(hConsole, color);
+		printf("%-3s = %d \n", flagMnem[i], (z80Cpu->additionalGpRegisters[F] >> i) & 1);
 	}
 	printf("\n");
 
-	
-	color = changed8[A] ? GREEN : GREY;
+	color = changedGp[A] ? GREEN : GREY;
 	SetConsoleTextAttribute(hConsole, color);
-	printf("A = 0x%02X \t", z80Cpu->basicGpRegisters[A]);
+	printf("A = 0x%02X \t\t\t", z80Cpu->basicGpRegisters[A]);
+	color = changedAddGp[A] ? GREEN : GREY;
+	SetConsoleTextAttribute(hConsole, color);
+	printf("A' = 0x%02X \n\n", z80Cpu->additionalGpRegisters[A]);
 
-	
-	color = changed8[I] ? GREEN : GREY;
+	color = changedGp[I] ? GREEN : GREY;
 	SetConsoleTextAttribute(hConsole, color);
 	printf("I = 0x%02X \t", z80Cpu->basicGpRegisters[I]);
 
-	
-	color = changed8[R] ? GREEN : GREY;
+	color = changedGp[R] ? GREEN : GREY;
 	SetConsoleTextAttribute(hConsole, color);
 	printf("R = 0x%02X", z80Cpu->basicGpRegisters[R]);
 
-	static const char* spMnem[4] = { "IX","IY","SP","PC" };
-	for (size_t i = 0; i < 4; i++)
+	static const char* spMnem[SP_REGISTERS16_SIZE] = { "IX","IY","SP","PC" };
+	for (size_t i = 0; i < SP_REGISTERS16_SIZE; i++)
 	{
 		
-		color = changed16[i] ? GREEN : GREY;
+		color = changedSp16[i] ? GREEN : GREY;
 		SetConsoleTextAttribute(hConsole, color);
 		if (i % 2 == 0) {
 			printf("\n\n");
@@ -94,7 +106,9 @@ void registersDump(Z80Cpu* z80Cpu) {
 
 	SetConsoleTextAttribute(hConsole, (WORD)GREY);
 	printf("\n\n\n");
-	memcpy(previous8, z80Cpu->basicGpRegisters, 8);
-	memcpy(previous8+8, z80Cpu->spRegisters8, 2);
-	memcpy(previous16, z80Cpu->spRegisters16, 8);
+
+	memcpy(previousGp, z80Cpu->basicGpRegisters, GP_REGISTERS_SIZE);
+	memcpy(previousAddGp, z80Cpu->additionalGpRegisters, GP_REGISTERS_SIZE);
+	memcpy(previousSp8, z80Cpu->spRegisters8, SP_REGISTERS8_SIZE);
+	memcpy(previousSp16, z80Cpu->spRegisters16, sizeof(unsigned short)* SP_REGISTERS16_SIZE);
 }
